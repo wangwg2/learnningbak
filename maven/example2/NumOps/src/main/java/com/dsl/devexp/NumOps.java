eee709439377c41d467a30b9d80786ec557ddb3f
wangwg2 <wangwg2@msn.com>
2018-04-07 05:46:01 +0800

    uu

--- maven/example2/NumOps/src/main/java/com/dsl/devexp/NumOps.java
+++ maven/example2/NumOps/src/main/java/com/dsl/devexp/NumOps.java
@@ -1,7 +1,6 @@
 package com.dsl.devexp;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class NumOps 
--- maven/helloweb/src/main/java/org/example/HelloServlet.java
+++ maven/helloweb/src/main/java/org/example/HelloServlet.java
@@ -11,8 +11,10 @@ import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
 
+  private static final long serialVersionUID = 1L;
+
   protected void doGet(HttpServletRequest request,
-                       HttpServletResponse response) throws ServletException, IOException
+                        HttpServletResponse response) throws ServletException, IOException
   {
       // Very simple - just return some plain text
       PrintWriter writer = response.getWriter();
